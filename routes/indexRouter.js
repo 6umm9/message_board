23478cfd829d3eab20c401840dd2985a5f80895e
6umm9 <tayseer.f01@gmail.com>
2026-01-04 18:45:33 +0300

    final commit finishing everything

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,11 +4,13 @@ const indexRouter = Router();
 
 const messages = [
     {
+        id: 0,
         text: "Hi there!",
         user: "Amando",
         added: new Date()
     },
     {
+        id: 1,
         text: "Hello World!",
         user: "Charles",
         added: new Date()
@@ -23,9 +25,19 @@ indexRouter.get("/new", (req, res) => {
     res.render("form", { title: "New Message" });
 });
 
+indexRouter.get("/message/:id", (req, res) => {
+    const message = messages.find(m => m.id === parseInt(req.params.id));
+    if (message) {
+        res.render("details", { title: "Message Details", message: message });
+    } else {
+        res.status(404).send("Message not found");
+    }
+});
+
 indexRouter.post("/new", (req, res) => {
     const { messageUser, messageText } = req.body;
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    const newId = messages.length > 0 ? Math.max(...messages.map(m => m.id)) + 1 : 0;
+    messages.push({ id: newId, text: messageText, user: messageUser, added: new Date() });
     res.redirect("/");
 });
 
